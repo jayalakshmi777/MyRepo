bf2beca851f9099ab7c5932a0ccce0a362c67d1f
jayal <jayal@LAPTOP-811ORES4>
2021-01-16 16:48:39 +0530

    Changed column

---
+++ Assignments/src/com/pac/Animal.java
@@ -0,0 +1,17 @@
+package com.pac;
+
+public class Animal{  
+	
+	 void eat(){System.out.println("animal is eating...");
+	 System.out.println("animal");}  
+	}  
+	  
+	class Dog extends Animal{  
+		
+	 void eat(){System.out.println("dog is eating...");}  
+	  
+	 public static void main(String args[]){  
+	  Animal a=new Dog();  
+	  a.eat();  
+	 }  
+	}  
