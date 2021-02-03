7b60ddb3a9f2960e9118c539a2ac78848d4efb1f
TL <TL@gmail.com>
2021-02-03 14:01:36 +0530

    Removed tostring method

--- SingletonDesignPattern/src/com/SingletonDesignPatern/Emp.java
+++ SingletonDesignPattern/src/com/SingletonDesignPatern/Emp.java
@@ -1,8 +1,11 @@
 package com.SingletonDesignPatern;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Emp {
 private int eid;
 private String ename;
+
 @Override
 public String toString() {
 	return "Emp [eid=" + eid + ", ename=" + ename + "]";
@@ -27,4 +30,6 @@ public void setEname(String ename) {
 	this.ename = ename;
 }
 
+
 }
+
